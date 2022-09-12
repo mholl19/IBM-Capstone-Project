9c64ccb01a9a1c96bb85fecbed33251e568a7a69
mholl19 <112111441+mholl19@users.noreply.github.com>
2022-09-12 16:01:01 -0700

    Create Capstone Shiny App UI.R

---
+++ Capstone Shiny App UI.R
@@ -0,0 +1,20 @@
+# Load required libraries
+require(leaflet)
+
+
+# Create a RShiny UI
+shinyUI(
+  fluidPage(padding=5,
+            titlePanel("Bike-sharing demand prediction app"), 
+            # Create a side-bar layout
+            sidebarLayout(
+              # Create a main panel to show cities on a leaflet map
+              mainPanel(
+                leaflet(elementId = 'city_bike_map', height = 600, width = 600)
+            
+              ),
+              # Create a side bar to show detailed plots for a city
+              sidebarPanel(
+                # select drop down list to select city
+              ))
+  ))
