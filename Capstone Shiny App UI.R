32f382f62d70dd9c6d6258664c0a9696c26f1e7f
mholl19 <112111441+mholl19@users.noreply.github.com>
2022-09-14 18:46:53 -0700

    Update Capstone Shiny App UI.R

--- Capstone Shiny App UI.R
+++ Capstone Shiny App UI.R
@@ -10,13 +10,16 @@ shinyUI(
             sidebarLayout(
               # Create a main panel to show cities on a leaflet map
               mainPanel(
-                leafletOutput('city_bike_map', height = 600, width = 800)
+                leafletOutput('city_bike_map', height = 500, width = 1000)
             
               ),
               # Create a side bar to show detailed plots for a city
               sidebarPanel(
-                selectInput(inputId = "city_dropdown", "Select City: ",
-                            c("All", "Seoul", "Suzhou", "London", "New York", "Paris"))
                 # select drop down list to select city
+                selectInput(inputId = "city_dropdown", "Select City: ",
+                            c("All", "Seoul", "Suzhou", "London", "New York", "Paris")),
+                plotOutput('temp_line', height = 300, width = 450),
+                plotOutput('bike_line', height = 300, width = 450, click ="plot_click"),
+                verbatimTextOutput("bike_date_output")
               ))
   ))
